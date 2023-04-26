fe237af7ae7925f2e3f031ab99659d559bc78ac0
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-04-26 16:25:57 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "postman-test-dev-79158",
-  url: "https://postman-test-dev-79158.botics.co",
-  api: "https://postman-test-dev-79158.botics.co/api/v1"
-}
+  "name": "postman-test-dev-79158",
+  "url": "https://postman-test-dev-79158.botics.co",
+  "api": "https://postman-test-dev-79158.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
