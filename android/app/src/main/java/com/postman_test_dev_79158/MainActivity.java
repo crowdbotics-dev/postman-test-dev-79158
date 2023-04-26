a81ddc8ebd81a66f15e7f87bb942ff3079ca8bc7
Crowdbotics <engineering@crowdbotics.com>
2023-04-26 09:01:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/postman_test_dev_79158/MainActivity.java
@@ -0,0 +1,15 @@
+package com.postman_test_dev_79158;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "postman_test_dev_79158";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "postman-test-dev-79158",
+  url: "https://postman-test-dev-79158.botics.co",
+  api: "https://postman-test-dev-79158.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
